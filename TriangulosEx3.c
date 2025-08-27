8470bfdc5769e0e46af2a194b8e001d98d9cab0f
AugustoWLeal <augustoleal@alunos.utfpr.edu.br>
2025-08-27 09:40:05 -0300

    Create TriangulosEx3.c

---
+++ TriangulosEx3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float a, b, c;
+	printf("Digite os 3 lados do triangulo: ");
+	scanf("%f %f %f",&a,&b,&c);
+	if ((a+b)<=c)
+		printf("Nao e um triangulo");
+	else if ((a+c)<=b)
+		printf("Nao e um triangulo");
+	else if ((b+c)<=a)
+		printf("Nao e um triangulo");
+	else if (a==b && a==c)
+		printf("O triangulo e equilatero");
+	else if(a!=b && a!=c && b!=c)
+		printf("O triangulo e escaleno");
+	else
+		printf("O triangulo C) isosceles");
+
+
+
+	return 0;
+}
